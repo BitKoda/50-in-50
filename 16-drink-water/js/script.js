6ae524aee69a01a4e60f85bb57132cd0d43885cd
bitkoda <jf.liv@pm.me>
2024-01-16 15:15:20 +0000

    complete project 16 - Drink Water

---
+++ 16-drink-water/js/script.js
@@ -0,0 +1,62 @@
+// --- //
+// Bug //  
+// --- //
+// If small cup 8 is marked full and then clicked again.
+
+// Uncaught TypeError:
+// Cannot read properties of null (reading 'classList')
+// at highlightCups (script.js:26:40)
+// at HTMLDivElement.<anonymous> (script.js:19:39)
+ 
+const smallCups = document.querySelectorAll('.cup-small');
+const litres = document.getElementById('litres');
+const percentage = document.getElementById('percentage');
+const remaining = document.getElementById('remaining');
+
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx))
+})
+
+function highlightCups(idx) {
+
+
+  if(smallCups[idx].classList.contains('full') && 
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--
+  }
+
+  smallCups.forEach((cup, cupIdx) => {
+    if(cupIdx <= idx) {
+      cup.classList.add('full')
+    } else {
+      cup.classList.remove('full')
+    }
+  })
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if(fullCups === 0) {
+    percentage.style.visibility = 'hidden'
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${fullCups / totalCups * 33}rem`
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+  }
+
+  if(fullCups === totalCups) {
+    remaining.style.visibility = 'hidden';
+    remaining.style.height = 0;
+  } else {
+    remaining.style.visibility = 'visible';
+    litres.innerText = `${2 - (250 * fullCups / 1000)}L`
+  }
+}
